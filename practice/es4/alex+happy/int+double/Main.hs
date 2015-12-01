af9a4ba61be2ef697ba15a2ce776820afe1b5db7
et3rminal <metallus.emanuele@gmail.com>
2015-12-01 23:09:18 +0100

    Modificato il Main, aggiunt commenti e un unwrap dell\'Either piu` corretto

--- practice/es4/alex+happy/int+double/Main.hs
+++ practice/es4/alex+happy/int+double/Main.hs
@@ -1,16 +1,30 @@
 
 module Main (main) where
 
+--Importo tutto quello che sta` in parsers/lexer
 import Lexer (alexScanTokens, Token(..))
 import ParserI (parseIntTree, IntTree(..))
 import ParserD (parseDoubleTree, DoubleTree(..))
 
+--data Either a b = Left a | Right b
+--either :: (a -> c) -> (b -> c) -> Either a b -> c
+--Quindi seleziona uno dei due e applica:
+--se Either a b è Left a, allora applica (a -> c) a
+--se Either a b è Right b allora applica (b -> c) b
 main = do
  x <- getContents
- print (either (show) (show) (parse (alexScanTokens x)))
+ (either (print) (print) (parse (alexScanTokens x)))
  
-parse :: [Token] -> Either IntTree DoubleTree
+--La magia dell'either! Non si possono restituire dati di due tipi diversi,
+--quindi si usa Either per incapsulare entrambi allo stesso tempo, ma alla fine ho uno o l'altro.
+--Nel main eseguo il print di quella che occorre, visto che print è una monad non gli interessa
+--che il tipo restituito da parse sia di un tipo o un altro, ma solo che derivi Show.
+parse :: [Token] -> Either IntTree DoubleTree 
 parse a@(x:_) = case x of
-  (Int _) -> (Left (parseIntTree a))
-  (Double _) -> (Right (parseDoubleTree a))
+  (Int _) -> Left (parseIntTree a)
+  (Double _) -> Right (parseDoubleTree a)
 
+--nella versione precedente usavo show al posto di print e printavo gli show, ma mi davano fastidio le \"\"
+--Si dovrà usare annotate discriminando se è Left o Right...complicazione, ma non ho idea per ora di come semplificare
+
+annotate :: Either IntTree DoubleTree -> 
