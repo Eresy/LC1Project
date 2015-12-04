b0adee7a7a4e0ac30b104c8788487c79549c4abe
Eresy <marco.bucchiarone@gmail.com>
2015-12-04 14:58:06 +0100

    still working on making es1 with user interaction

---
+++ ProgettoLC1 parte1 Gruppo 14/es1/es1.hs
@@ -0,0 +1,22 @@
+module Main where 
+
+import System.Environment
+
+data BST a = Void | Node a (BST a) (BST a)
+  deriving (Eq, Ord, Show, Read)
+
+boundedMaximum :: Ord a => a -> [BST a] -> [a]
+boundedMaximum _ [] = []
+
+boundedMaximum n (x:xs) = (maxNT n x) ++ ( boundedMaximum n xs)
+  where
+    maxNT n x = foldBST (max) (getmin n x)
+      where
+        foldBST f [] = []
+        foldBST f list = [ foldl1 f list ]
+        getmin n Void  = []
+        getmin n (Node m l r) | n <= m = getmin n l
+                              | n >= m = [m] ++ (getmin n l) 
+
+main = do args <- getArgs
+          mapM_ putStrLn args
