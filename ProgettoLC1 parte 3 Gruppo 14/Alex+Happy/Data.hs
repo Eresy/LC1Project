da43aef24097dac55a26a9ba34e0e7c094012724
Emanuele Tonetti <metallus.emanuele@gmail.com>
2016-04-27 11:27:17 +0200

    Nuovi tipi di dato

--- ProgettoLC1 parte 3 Gruppo 14/Alex+Happy/Data.hs
+++ ProgettoLC1 parte 3 Gruppo 14/Alex+Happy/Data.hs
@@ -2,8 +2,8 @@ module Data where
 
 data Token =	Type String	| BrakOpen | BrakClose | SBrakOpen 
 				| SBrakClose | CBrakOpen | CBrakClose | AssignOp 
-				| AddOp | SubOp | MulOp | DivOp | CatOp | Comma 
-				| Semicolon
+				| AddOp | SubOp | MulOp | DivOp | Comma | Semicolon
+				| CompOp | NegOp | IncOp | DecOp
 
 data Type = 	Integer | Float | Character | String | Pointer Type 
 				| Array Type
