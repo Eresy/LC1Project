f17c17378556369a9cb3993c893886c707ebe884
Eresy <marco.bucchiarone@gmail.com>
2016-05-04 14:17:30 +0200

    retabbed Data.hswq

--- ProgettoLC1 parte3 Gruppo 14/Alex+Happy/Data.hs
+++ ProgettoLC1 parte3 Gruppo 14/Alex+Happy/Data.hs
@@ -1,60 +1,60 @@
 module Data where
 
-data Token =	Int String Pos
-		| Double String Pos
-		| Char String Pos
-		| String String Pos
-		| Label String Pos
-		| BrakOpen Pos
-		| BrakClose Pos
-		| SBrakOpen Pos
-		| SBrakClose Pos
-		| CBrakOpen Pos
-		| CBrakClose Pos
-		| AssignOp Pos
-		| EqualsOp Pos
-		| DiffOp Pos
-		| LessThanOp Pos
-		| GreaterThanOp Pos
-		| ELessThanOp Pos
-		| EGreaterThanOp Pos
-		| AddOp Pos
-		| SubOp Pos
-		| MulOp Pos
-		| DivOp Pos
-		| IncOp Pos
-		| DecOp Pos
-		| Comma Pos
-		| Semicolon Pos
-		| Dereference Pos
-		| And Pos
-		| Or Pos
-		| NegOp Pos
-		| Break Pos
-		| Continue Pos
-		| Return Pos
-		| IntLabel Pos
-		| FloatLabel Pos
-		| CharLabel Pos
-		| StringLabel Pos
-		| VoidLabel Pos
-		| ReadIntPF Pos
-		| WriteIntPF Pos
-		| ReadFloatPF Pos
-		| WriteFloatPF Pos
-		| ReadCharPF Pos
-		| WriteCharPF Pos
-		| ReadStringPF Pos
-		| WriteStringPF Pos
-		| ValRes Pos
-		deriving (Eq, Ord, Show)
-		
+data Token =    Int String Pos
+                | Double String Pos
+                | Char String Pos
+                | String String Pos
+                | Label String Pos
+                | BrakOpen Pos
+                | BrakClose Pos
+                | SBrakOpen Pos
+                | SBrakClose Pos
+                | CBrakOpen Pos
+                | CBrakClose Pos
+                | AssignOp Pos
+                | EqualsOp Pos
+                | DiffOp Pos
+                | LessThanOp Pos
+                | GreaterThanOp Pos
+                | ELessThanOp Pos
+                | EGreaterThanOp Pos
+                | AddOp Pos
+                | SubOp Pos
+                | MulOp Pos
+                | DivOp Pos
+                | IncOp Pos
+                | DecOp Pos
+                | Comma Pos
+                | Semicolon Pos
+                | Dereference Pos
+                | And Pos
+                | Or Pos
+                | NegOp Pos
+                | Break Pos
+                | Continue Pos
+                | Return Pos
+                | IntLabel Pos
+                | FloatLabel Pos
+                | CharLabel Pos
+                | StringLabel Pos
+                | VoidLabel Pos
+                | ReadIntPF Pos
+                | WriteIntPF Pos
+                | ReadFloatPF Pos
+                | WriteFloatPF Pos
+                | ReadCharPF Pos
+                | WriteCharPF Pos
+                | ReadStringPF Pos
+                | WriteStringPF Pos
+                | ValRes Pos
+                deriving (Eq, Ord, Show)
+                
 type Pos = ( Int , Int )
 
-data Type =	IntType Int
-		| DoubleType Double 
-		| CharType Char
-		| StringType String
-		| PointerType Type 
-		| ArrayType Type
-		deriving(Eq, Ord, Show)
+data Type =     IntType Int
+                | DoubleType Double 
+                | CharType Char
+                | StringType String
+                | PointerType Type 
+                | ArrayType Type
+                deriving(Eq, Ord, Show)
